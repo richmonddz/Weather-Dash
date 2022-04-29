0462f82983804dd31731325fe2aeb1859cda0887
Richmond Dominguez <richmonddominguez@gmail.com>
2022-04-29 01:40:59 -0700

    basic html using bootstrap and var created in js

---
+++ script.js
@@ -0,0 +1,7 @@
+var cityCon = document.querySelector("#citySearch");
+var cityChoice = document.querySelector("#city-selection");
+var mainWeather = document.querySelector("#weather-box");
+var cityInpt = document.querySelector("#city");
+var cityForecast = document.querySelector("#forecast");
+var forcastBox = document.querySelector("#forecastBoxes");
+var pastCities = document.querySelector("#prevCities");
