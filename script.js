7b892089827242d86c5e6c705a8d6fe0dd6b6200
Richmond Dominguez <richmonddominguez@gmail.com>
2022-04-29 21:20:42 -0700

    more js added

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ var cityInpt = document.querySelector("#city");
 var cityForecast = document.querySelector("#forecast");
 var forcastBox = document.querySelector("#forecastBoxes");
 var pastCities = document.querySelector("#prevCities");
+
+var getWeather = function (city) {
+  var apiKey = "7d0170c82115fd50125866009a57c3f4";
+  var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  fetch(url).then(function (response) {
+    response.json().then(function (data) {
+      showWeather(data, city);
+    });
+  });
+};
